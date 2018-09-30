fixed k means init, deleted softmax and cosine angle init from code base, display init method upon execution

miace/mi_target.py
@@ -8,16 +8,9 @@ default_parameters = {
     "methodFlag": True,
     # Set to 1 to use global mean and covariance, set to 0 to use negative bag mean and covariance
     "globalBackgroundFlag": False,
-
-    #Options: 1,2,3
-    '''
-    Type 1 is to use best positive instance based on objective function value,
-    Type 2 is to select positive instance with smallest cosine similarity with negative instance mean,
-    Type 3 clusters the data with k-means and selects the best cluster center as the initial target signature
-    '''
+    #Type 1 is to use best positive instance based on objective function value,
+    #Type 2 clusters the data with k-means and selects the best cluster center as the initial target signature
     "initType": 1,
-    # Set to 0 to use max, set to 1 to use softmax in computation of objective function values of positive bags
-    "softmaxFlag": False,
     # Value used to indicate positive bags, usually 1
     "posLabel": 1,
     # Value used to indicate negative bags, usually 0 or -1
@@ -46,18 +39,14 @@ def mi_target(data_bags, labels, parameters=default_parameters):
 
       parameters - struct - The struct contains the following fields:
         1. parameters.methodFlag: Set to 0 for MI-SMF, Set to 1 for MI-ACE
-        2. parameters.initType: Options are 1, 2, or 3.
+        2. parameters.initType: Options are 1 or 2
         3. parameters.globalBackgroundFlag: set to 1 to use global mean and covariance, set to 0 to use negative bag mean and covariance
-        4. parameters.softmaxFlag: Set to 0 to use max, set to
-            1 to use softmax in computation of objective function
-            values of positive bags  (This is generally not used
-            and fixed at 0)
-        5. parameters.posLabel: Value used to indicate positive
+        4. parameters.posLabel: Value used to indicate positive
         bags, usually 1
-        6. parameters.negLabel: Value used to indicate negative bags, usually 0 or -1
-        7. parameters.maxIter: Maximum number of iterations (rarely used)
-        8. parameters.samplePor: If using init1, percentage of positive data points used to initialize (default = 1)
-        9. parameters.initK = 1000; % If using init3, number of clusters used to initialize (default = 1000);
+        5. parameters.negLabel: Value used to indicate negative bags, usually 0 or -1
+        6. parameters.maxIter: Maximum number of iterations (rarely used)
+        7. parameters.samplePor: If using init1, percentage of positive data points used to initialize (default = 1)
+        8. parameters.initK = 1000; % If using init3, number of clusters used to initialize (default = 1000);
     Outputs:
       opt_target - estimated target concept
       opt_obj_val -  Final Objective Function value
@@ -65,7 +54,7 @@ def mi_target(data_bags, labels, parameters=default_parameters):
       sig_inv_half - Square root of background covariance, Use sig_inv_half'*sig_inv_half as covariance in ACE or SMF detector with test data
       init_t - initial target concept
     """
-    print(f'Data bags shape: {data_bags.shape}\nLabels shape: {labels.shape}') # data bag shape is bag x pixel x bands, label is 1 x bag
+    # print(f'Data bags shape: {data_bags.shape}\nLabels shape: {labels.shape}') # data bag shape is bag x pixel x bands, label is 1 x bag
     num_pos_bags = np.sum(labels == parameters["posLabel"])
     negLabels = (labels == parameters["negLabel"])
     negLabels = np.reshape(negLabels, newshape=(negLabels.shape[1]))
@@ -99,8 +88,8 @@ def train_target_signature(whitened_data, labels, parameters, num_pos_bags):
     pos_databags = whitened_data[posLabels]
     neg_databags = whitened_data[negLabels]
 
-    print('Initializing...')
     init = init_function(parameters['initType'])
+    print(f'Initializing with {init.__name__}...')
 
     init_t, opt_obj_val, pos_bags_max = init(
         pos_databags, neg_databags, parameters)
@@ -130,7 +119,7 @@ def train_target_signature(whitened_data, labels, parameters, num_pos_bags):
 
         # Update Objective and Determine the max points in each bag
         opt_obj_val, pos_bags_max = eval_objective_whitened(
-            pos_databags, neg_databags, opt_target, parameters['softmaxFlag'])
+            pos_databags, neg_databags, opt_target)
 
         # see if objective value has been reached
         if np.any(objective_val == opt_obj_val):
@@ -149,7 +138,7 @@ def train_target_signature(whitened_data, labels, parameters, num_pos_bags):
     return opt_target, opt_obj_val, init_t
 
 
-def eval_objective_whitened(pos_databags, neg_databags, target, softmax_flag):
+def eval_objective_whitened(pos_databags, neg_databags, target):
     num_dim = pos_databags[0].shape[1] #number of bands
 
     pos_conf_bags = np.zeros((pos_databags.shape[0], 1))
@@ -157,17 +146,11 @@ def eval_objective_whitened(pos_databags, neg_databags, target, softmax_flag):
 
     for i, pos_data in enumerate(pos_databags):
         pos_conf = np.sum(pos_data*target, axis=1) #(10,)
-        if softmax_flag:
-            w = np.exp(pos_conf) / np.sum(np.exp(pos_conf))
-            # w = np.reshape(w, (w.shape[0],1))
-            pos_conf_bags[i] = np.sum(pos_conf * w)
-            pos_conf_max[i] = np.sum(w * pos_data, axis=0)
-        else:
-            idx = np.argmax(pos_conf)
-            max_conf = pos_conf[idx]
+        idx = np.argmax(pos_conf)
+        max_conf = pos_conf[idx]
 
-            pos_conf_bags[i] = max_conf
-            pos_conf_max[i] = pos_data[idx]
+        pos_conf_bags[i] = max_conf
+        pos_conf_max[i] = pos_data[idx]
 
     neg_conf_bags = np.zeros((neg_databags.shape[0], 1))
 
@@ -180,9 +163,9 @@ def eval_objective_whitened(pos_databags, neg_databags, target, softmax_flag):
 
 
 def init_function(initType=1):
-    init_functions = [exhaustive_init, cosine_angle_init, kmeans_init]
+    init_functions = [exhaustive_init, kmeans_init]
     if initType > len(init_functions):
-        raise ValueError("Please provide a value between 1 and 3 for initType")
+        raise ValueError("Please provide a value of 1 or 2 for initType")
     return init_functions[initType - 1]
 
 
@@ -203,7 +186,7 @@ def exhaustive_init(pos_databags, neg_databags, parameters):
     temp_obj_val = np.zeros(pos_data_reduced.shape[0])
     for i, opt_target in enumerate(pos_data_reduced):
         temp_obj_val[i], _ = eval_objective_whitened(
-            pos_databags, neg_databags, opt_target, parameters['softmaxFlag'])
+            pos_databags, neg_databags, opt_target)
 
     # optimal target
     idx = np.argmax(temp_obj_val)
@@ -214,36 +197,7 @@ def exhaustive_init(pos_databags, neg_databags, parameters):
     init_t = opt_target
 
     opt_obj_val, pos_bags_max = eval_objective_whitened(
-        pos_databags, neg_databags, opt_target, parameters['softmaxFlag'])
-
-    return init_t, opt_obj_val, pos_bags_max
-
-
-def cosine_angle_init(pos_databags, neg_databags, parameters):
-    # smallest cosine vector angle initialization
-    # init2
-    pos_data = flatten_databags(pos_databags)
-    neg_data = flatten_databags(neg_databags)
-    n_dim = pos_data.shape[1]
-
-    # make data orthonormal
-    pos_denom = np.sqrt(np.sum(pos_data * pos_data, axis=1))
-    neg_denom = np.sqrt(np.sum(neg_data * neg_data, axis=1))
-
-    pos_data /= pos_denom
-    neg_data /= neg_denom
-
-    neg_mean = np.mean(neg_data, axis=0)
-
-    temp_obj_val = np.sum(pos_data * neg_mean, axis=1)
-    idx = np.argmin(temp_obj_val)
-    opt_target = pos_data[idx]
-
-    opt_target /= np.linalg.norm(opt_target)
-    init_t = opt_target
-
-    opt_obj_val, pos_bags_max = eval_objective_whitened(
-        pos_databags, neg_databags, opt_target, parameters['softmaxFlag'])
+        pos_databags, neg_databags, opt_target)
 
     return init_t, opt_obj_val, pos_bags_max
 
@@ -256,25 +210,23 @@ def kmeans_init(pos_databags, neg_databags, parameters):
     if 'C' in parameters:
         C = parameters['C']
     else:
-        k_means = KMeans(n_clusters=min(
-            len(pos_data), parameters['initK']), max_iter=parameters['maxIter'])
+        k_means = KMeans(n_clusters=min(len(pos_data), parameters['initK']), max_iter=parameters['maxIter'])
         C = k_means.fit(pos_data)
 
-    temp_obj_val = np.zeros(len(C))
+    temp_obj_val = [None] * len(C.labels_)
 
     # Loop through cluster centers
-    for i, centroid in enumerate(C):
+    for i, centroid in enumerate(C.cluster_centers_):
         opt_target = centroid / np.linalg.norm(centroid)
-        temp_obj_val[i] = eval_objective_whitened(
-            pos_databags, neg_databags, opt_target, parameters['softmaxFlag'])
+        temp_obj_val[i], _ = eval_objective_whitened(pos_databags, neg_databags, opt_target)
 
     idx = np.argmax(temp_obj_val)
-    opt_target = C[idx]
+    opt_target = C.cluster_centers_[idx]
     opt_target /= np.linalg.norm(opt_target)
 
     init_t = opt_target
     opt_obj_val, pos_bags_max = eval_objective_whitened(
-        pos_databags, neg_databags, opt_target, parameters['softmaxFlag'])
+        pos_databags, neg_databags, opt_target)
 
     return init_t, opt_obj_val, pos_bags_max
 
@@ -293,15 +245,12 @@ def whiten_data(b_cov, data_bags, b_mu, parameters=default_parameters):
     if parameters['methodFlag']:
         denom = [np.sqrt(np.sum(m_scale[i]*m_scale[i], axis=1)) for i in range(m_scale.shape[0])]
         denom = np.array([np.reshape(denom[bag], (denom[bag].shape[0], 1)) for bag in range(m_scale.shape[0])])
-        print(f'denom after: {denom[0].shape}')
+        # print(f'denom after: {denom[0].shape}')
         whitened_data = np.asarray([np.divide(m_scale[bag], denom[bag]) for bag in range(data_bags.shape[0])])
 
     else:
         denom = 1.0
         whitened_data = np.divide(m_scale, denom)
-
-    # print('white: ', np.mean(np.vstack(whitened_data[:])), whitened_data.shape)
-    # print(f'white: {np.mean(whitened_data)}, {whitened_data.shape}')
 
     return whitened_data, sig_inv_half, s, v
 